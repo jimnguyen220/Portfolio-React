1daff8f1cbd31029a7914f6673effbc17f9453a1
Jim Nguyen <jimnguyen220@gmail.com>
2021-05-01 23:07:11 -0500

    initial commit

---
+++ portfolio-react/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import './App.css';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
